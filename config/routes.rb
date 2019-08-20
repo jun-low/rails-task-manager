57382970f0415fedd4072633f4b59232c970fb68
jun low <zhijun.low@gmail.com>
2019-08-20 15:04:06 +1000

    Create a new task app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'tasks', to: 'tasks#index'
+  resources :tasks
+  # only: [:index, :show, :new, :edit, :create, :update, :destroy]
 end
